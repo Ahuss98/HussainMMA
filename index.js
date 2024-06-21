aae6ea2ba4d271e77c63810578710bf13480d512
Ahmed Hussain <ahmedhussain@Ahmeds-MacBook-Pro.local>
2024-06-21 20:05:16 +0100

    added link to workout api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //need to make a website for hussainFitness to display the company
 //also need to build a workout plan builder which will take some work
 //use api ninjas for workout api and database of workouts
+//https://api-ninjas.com/api/exercises this is the api link
 //then ask user for prefrences 
 //set up how many days a week 
 //let user select from exercises and add to the list then add to a calender
